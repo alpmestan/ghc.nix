748a7e2beffda5d88e5c7ebf8a72c5e4b93bd490
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2019-02-27 23:55:02 +0300

    Drop ineffective flags and derivation arguments

--- default.nix
+++ default.nix
@@ -16,7 +16,6 @@ in
 , withDocs  ? true
 , withDwarf ? nixpkgs.stdenv.isLinux  # enable libdw unwinding support
 , withNuma  ? nixpkgs.stdenv.isLinux
-, mkFile    ? null
 , cores     ? 4
 }:
 
@@ -78,29 +77,22 @@ in
   packages    = pkgset: [ hsdrv ];
   buildInputs = depsSystem;
 
-  hardeningDisable = [ "fortify" ];
-  postPatch = "patchShebangs .";
-  preConfigure = ''
-    echo Running preConfigure...
-    echo ${version} > VERSION
-    ((git log -1 --pretty=format:"%H") || echo dirty) > GIT_COMMIT_ID
-    ./boot
-  '' + stdenv.lib.optionalString (mkFile != null) ''
-    cp ${mkFile} mk/build.mk
-  '';
   # N.B. CC gets overridden by stdenv
   CC                  = "${stdenv.cc}/bin/cc"        ;
   CC_STAGE0           = CC                           ;
   CFLAGS              = "-I${env}/include"           ;
   CPPFLAGS            = "-I${env}/include"           ;
+  CURSES_INCLUDE_DIRS = "${env}/include"             ;
+  CURSES_LIB_DIRS     = "${env}/lib"                 ;
+  GMP_INCLUDE_DIRS    = "${env}/include"             ;
+  GMP_LIB_DIRS        = "${env}/lib"                 ;
   LDFLAGS             = "-L${env}/lib"               ;
   LD_LIBRARY_PATH     = "${env}/lib"                 ;
-  GMP_LIB_DIRS        = "${env}/lib"                 ;
-  GMP_INCLUDE_DIRS    = "${env}/include"             ;
-  CURSES_LIB_DIRS     = "${env}/lib"                 ;
-  CURSES_INCLUDE_DIRS = "${env}/include"             ;
-  configureFlags      = lib.concatStringsSep " "
-    ( lib.optional withDwarf "--enable-dwarf-unwind" ) ;
+  # Without this, we see a whole bunch of warnings about LANG, LC_ALL and locales in general.
+  # In particular, this makes many tests fail because those warnings show up in test outputs too...
+  # The solution is from: https://github.com/NixOS/nix/issues/318#issuecomment-52986702
+  LOCALE_ARCHIVE      = if stdenv.isLinux then "${glibcLocales}/lib/locale/locale-archive" else "";
+  NIX_BUILD_CORES     = cores;
 
   shellHook           = let toYesNo = b: if b then "YES" else "NO"; in ''
     # somehow, CC gets overriden so we set it again here.
@@ -119,22 +111,12 @@ in
     echo "    CFLAGS          = $CFLAGS"
     echo "    CPPFLAGS        = $CPPFLAGS"
     echo "    LDFLAGS         = $LDFLAGS"
-    echo "    LD_LIBRARY_PATH = ${env}/lib"
+    echo "    LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
     echo "    LLVM            = ${toYesNo withLlvm}"
     echo "    libdw           = ${toYesNo withDwarf}"
     echo "    numa            = ${toYesNo withNuma}"
-    echo "    configure flags = ${configureFlags}"
     echo
     echo Please report bugs, problems or contributions to
     echo https://github.com/alpmestan/ghc.nix
   '';
-  enableParallelBuilding = true;
-  NIX_BUILD_CORES = cores;
-  stripDebugFlags = [ "-S" ];
-
-  # Without this, we see a whole bunch of warnings about LANG, LC_ALL and locales in general.
-  # In particular, this makes many tests fail because those warnings show up in test outputs too...
-  # The solution is from: https://github.com/NixOS/nix/issues/318#issuecomment-52986702
-  LOCALE_ARCHIVE = if stdenv.isLinux then "${glibcLocales}/lib/locale/locale-archive" else "";
-
 })
