c3cbe4edce1b2f04adf649b35503e0a4c5abe8f1
Ben Gamari <ben@smart-cactus.org>
2019-12-06 16:48:06 -0500

    Add missing latex dependencies

--- default.nix
+++ default.nix
@@ -43,7 +43,10 @@ let
 
     ourtexlive =
       nixpkgs.texlive.combine {
-        inherit (nixpkgs.texlive) scheme-medium collection-xetex fncychap titlesec tabulary varwidth framed capt-of wrapfig needspace dejavu-otf helvetic; };
+        inherit (nixpkgs.texlive)
+          scheme-medium collection-xetex fncychap titlesec tabulary varwidth
+          framed capt-of wrapfig needspace dejavu-otf helvetic upquote;
+      };
     fonts = nixpkgs.makeFontsConf { fontDirectories = [ nixpkgs.dejavu_fonts ]; };
     docsPackages = if withDocs then [ python3Packages.sphinx ourtexlive ] else [];
 
