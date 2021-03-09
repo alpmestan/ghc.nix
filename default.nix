9adaf8abe53fa0618c1561919ddfbc4342fe144b
Cheng Shao <cheng.shao@tweag.io>
2021-03-09 10:38:58 +0000

    Change default bootghc to ghc8102

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let
   sources = import ./nix/sources.nix {};
 in
 { nixpkgs   ? import (sources.nixpkgs) {}
-, bootghc   ? "ghc884"
+, bootghc   ? "ghc8102"
 , version   ? "9.1"
 , hadrianCabal ? (builtins.getEnv "PWD") + "/hadrian/hadrian.cabal"
 , nixpkgs-unstable ? import (sources.nixpkgs-unstable) {}
