b1d54cd609a582a8f341ae3835df32173ffeb40e
mangoiv <mail@mangoiv.com>
2023-02-07 22:44:50 +0100

    [feat] check formatting in CI, document inputs to ghc.nix, restore old usage of shell.nix, add more documentation to README, ignore results

--- flake.nix
+++ flake.nix
@@ -15,25 +15,34 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, all-cabal-hashes, ... }: with nixpkgs.lib; let
-    supportedSystems = nixpkgs.lib.systems.flakeExposed;
+  outputs = { self, nixpkgs, nixpkgs-unstable, all-cabal-hashes, ... }: with nixpkgs.lib; let
+    supportedSystems = systems.flakeExposed;
     perSystem = genAttrs supportedSystems;
     pkgsFor = system: import nixpkgs { inherit system; };
     unstablePkgsFor = system: import nixpkgs-unstable { inherit system; };
+
+    defaultSettings = system: {
+      inherit system;
+      all-cabal-hashes = all-cabal-hashes.outPath;
+      nixpkgs = pkgsFor system;
+      nixpkgs-unstable = unstablePkgsFor system;
+    };
+
+    # NOTE: change this according to the settings allowed in the ./ghc.nix file and described 
+    # in the `README.md`
+    userSettings = {
+      withHadrianDeps = true;
+      withIde = true;
+    };
   in
   {
     devShells = perSystem (system: rec {
-      ghc-nix = import ./ghc.nix {
-        inherit system;
-        all-cabal-hashes = all-cabal-hashes.outPath;
-        withHadrianDeps = true;
-        withIde = true;
-        nixpkgs = pkgsFor system;
-        nixpkgs-unstable = unstablePkgsFor system;
-      };
-
+      ghc-nix = import ./ghc.nix (defaultSettings system // userSettings);
       default = ghc-nix;
     });
     formatter = perSystem (system: (pkgsFor system).nixpkgs-fmt);
+
+    # NOTE: this attribute is used by the flake-compat code to allow passing arguments to ./ghc.nix
+    legacy = args: import ./ghc.nix (defaultSettings args.system // args);
   };
 }
