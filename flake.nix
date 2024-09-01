78e80fb5a7ccca410443385316429b005e59d7aa
Sven Tennie <sven.tennie@gmail.com>
2024-09-01 12:14:50 +0200

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
